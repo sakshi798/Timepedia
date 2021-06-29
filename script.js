9cd7d840c39a503e8ddc576b17c7d23ffe31e0a3
Sakshi Singh <sakshisingh7985252@gmail.com>
2021-06-30 04:56:27 +0530

    Timepedia project

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";  
+
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var day=date.getDate();  
+  var month=date.getMonth()+1;  
+  var year=date.getFullYear();  
+  var time = " Date is: "+ day +"/" + month + "/" + year ;
+  var date =  h + ":" + m + ":" + s + " " + session ;  
+  document.getElementById("MyClockDisplay").innerHTML = date;
+  document.getElementById("MydateDisplay").innerHTML = time;
+  document.title = date;
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
+
